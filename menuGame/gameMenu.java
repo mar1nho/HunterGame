05a8c2c4636e699fd77f8f004932781c796aa173
Marinho <120027666+mar1nho@users.noreply.github.com>
2023-04-07 17:43:25 -0300

    important changes
    i just updated the menu page

--- menuGame/gameMenu.java
+++ menuGame/gameMenu.java
@@ -1,30 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package menuGame;
-
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
+import javax.swing.*;
 
 public class gameMenu extends JFrame {
     
-    JLabel label = new JLabel();
-    int y = 20;
-    Color corDeFundo = new Color(212,239,223);
-    
+        Image img;
+        JLabel label = new JLabel();
+        Color corDeFundo = new Color(212,239,223);
     
     public gameMenu() throws InterruptedException {
+        
         //Tamanho da tela e afins
         setTitle("Hunting for Life");
         setSize(500, 500);
@@ -33,73 +19,61 @@ public class gameMenu extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null);
         setBackground(Color.RED);
-       
-        
-    //Cria o logo em ASCII
-    for(int j = 0; j < 12; j++){
-        String[] menuIcon = {" ___            ___","/      \\          /      \\","\\_     \\        /  __/"," _\\     \\      /  /__"," \\___  \\__/  __/","     \\_          \\_","       | @ @  \\_","       |","     _/     /\\","    /o)  (o/\\ \\_","    \\_____/ /","      \\____/"};
-        label = new JLabel(menuIcon[j]);
-        y += 15;
-        label.setBounds(190, y ,400,50);
-        label.setFont(new Font("Arial", Font.BOLD, 15));     
-        add(label);
-        setVisible(true);
-            if(j != 0){
-                Thread.sleep(500);
+
+        //Adiciona a imagem do alce
+        ImageIcon imagemOriginal = new ImageIcon(getClass().getResource("/moose.png"));
+        Image imagemRedimensionada = imagemOriginal.getImage().getScaledInstance(160, 200, Image.SCALE_SMOOTH);
+        ImageIcon imagem = new ImageIcon(imagemRedimensionada);
+        JLabel imgLabel = new JLabel(imagem);
+        JPanel painelImg = new JPanel();
+        painelImg.add(imgLabel);
+        painelImg.setBounds(165,95,160,200);
+        painelImg.setOpaque(false);
+        add(painelImg);
+
+        //Título
+        JLabel title = new JLabel("Hunting for Life");
+        title.setBounds(153, -15, 400, 100);
+        title.setFont(new Font("Arial", Font.BOLD, 25));
+        add(title);
+
+        //INTRODUÇÃO
+        JLabel introd = new JLabel("Bem-Vindo ao jogo de caça");
+        introd.setBounds(150, 10, 500, 100);
+        introd.setFont(new Font("Arial", Font.BOLD, 15));
+        introd.setForeground(Color.GREEN);
+        add(introd);
+
+        // Botões //
+
+        // Instruções
+        JButton info = new JButton("Instruções");
+        info.setBounds(270,340,100,40);
+        info.setBackground(Color.GREEN);
+        info.setForeground(Color.BLACK);
+        info.setFocusPainted(false);
+        info.addActionListener(this::infos); // Ao abertar o botão, executa o metodo criado
+        add(info);
+
+        //Iniciar, vai pra página de criação de personagem
+        JButton iniciar = new JButton("Jogar");
+        iniciar.setBounds(120,340,100,40);
+        iniciar.setBackground(Color.GREEN);
+        iniciar.setForeground(Color.BLACK);
+        iniciar.setFocusPainted(false);
+        iniciar.addActionListener(new ActionListener(){ // Adiciona um evento ao apertar o botão
+            @Override
+            public void actionPerformed(ActionEvent e){
+                Criacao telaDeCriacao = new Criacao(); // Cria uma variável para a tela de criação de personagem
+                telaDeCriacao.setVisible(true);  // Habilita a tela de criação
                 setVisible(false);
             }
-        Thread.sleep(500);
-        
+        });
+        add(iniciar);
+        fundoDeTela();
+        setVisible(true);
     }
-    
-    //Título
-   
-    
-    
-    JLabel title = new JLabel("Hunting for Life");
-    title.setBounds(160, -15, 400, 100);
-    title.setFont(new Font("Arial", Font.BOLD, 25));
-    add(title);
-    
-    //INTRODUÇÃO
-    JLabel introd = new JLabel("Bem-Vindo ao jogo de caça de texto. Selecione abaixo as opções:");
-    introd.setBounds(40, 240, 500, 100);
-    introd.setFont(new Font("Arial", Font.PLAIN, 15)); 
-    add(introd);
-    
-    // Botões
-    
-    JButton info = new JButton("Instruções");
-    info.setBounds(270,340,100,40);
-    info.setBackground(Color.GREEN);
-    info.setForeground(Color.BLACK);
-    info.setFocusPainted(false);
-    info.addActionListener(this::infos);
-    add(info);
-    
-    JButton iniciar = new JButton("Jogar");
-    iniciar.setBounds(120,340,100,40);
-    iniciar.setBackground(Color.GREEN);
-    iniciar.setForeground(Color.BLACK);
-    iniciar.setFocusPainted(false);
-    iniciar.addActionListener(new ActionListener(){
-        @Override
-        public void actionPerformed(ActionEvent e){
-            Criacao telaDeCriacao = new Criacao();
-            telaDeCriacao.setVisible(true);
-            setVisible(false);
-        }
-    });
-    add(iniciar);
-    
-    
-    
-            
-    corDeFundo();
-    setVisible(true);
-    }
-    
- 
+
     private void infos(ActionEvent event){
         JOptionPane.showMessageDialog(null, "Olá Jogador", "Inicio do Jogo", JOptionPane.QUESTION_MESSAGE);
         JOptionPane.showMessageDialog(null, "Deve ter dúvidas de como jogar esse jogo", "Inicio do Jogo", JOptionPane.QUESTION_MESSAGE);
@@ -109,18 +83,21 @@ public class gameMenu extends JFrame {
         JOptionPane.showMessageDialog(null, "Toda vez que você dome um dia se passa, e ao chegar no 10º dia vivo, você receberá uma missão.", "Instruções", JOptionPane.WARNING_MESSAGE);
         JOptionPane.showMessageDialog(null,"Chegue ao 15º dia vivo e ganhe o jogo");
     }
-    
-    
-    
-    
-    
-    
-    public void corDeFundo(){
-        JPanel corMenu = new JPanel();
-        corMenu.setBackground(corDeFundo);
-        corMenu.setBounds(0,0,500,500);
-        add(corMenu);
+
+    public void fundoDeTela(){
+        //Adiciona o fundo de tela com o alce.
+        ImageIcon fundo = new ImageIcon(getClass().getResource("/fundo.jpg"));
+        Image imagemRedimensionada = fundo.getImage().getScaledInstance(500,500, Image.SCALE_SMOOTH);
+        ImageIcon fundoMenu = new ImageIcon(imagemRedimensionada);
+        JLabel menuFundo = new JLabel(fundoMenu);
+        JPanel menu = new JPanel();
+        menu.add(menuFundo);
+        menu.setBackground(corDeFundo);
+        menu.setBounds(0,0,500,500);
+        add(menu);
     }
-    
-   
 }
+
+
+
+
