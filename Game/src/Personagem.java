8b2364515b25b2dc12ddfb996658f6027e27497e
Marinho <120027666+mar1nho@users.noreply.github.com>
2023-04-11 22:47:39 -0300

    clean

--- Game/src/Personagem.java
+++ Game/src/Personagem.java
@@ -3,20 +3,15 @@ import java.awt.*;
 public class Personagem {
 
     Criacao criacao = new Criacao();
-    String nome = criacao.getNomeDoPersonagem();
-    int idade = criacao.getIdade();
-    int carisma = criacao.getCarismaPoints();
-    int sorte = criacao.getLuckyPoints();
     Image imagem = criacao.imagem();
-    
-    String nomeEscolhido = nome;
-    int carismaPoints = carisma;
-    int luckyPoints = sorte;
-    int idadeDoPersonagem = idade;
-        int dinheiro = 50;
-        int diasPassados = 0;
-        int sono = 2;
-        int fome = 10;
-        int energia = 7;
-}
+    Image yuri = criacao.yuri();
 
+    String[] inventario = { "Comida", "Vazio"};
+    String nomeEscolhido = criacao.getNomeDoPersonagem();
+    int carismaPoints = criacao.getCarismaPoints();
+    int luckyPoints = criacao.getLuckyPoints();
+    int idadeDoPersonagem = criacao.getIdade();
+        int diasPassados = 0;
+        int fome = 80;
+        int energia = 100;
+}
